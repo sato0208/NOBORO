89089c33d5d95ceec6ff520abc4b2ba43323de49
sato chiharu <kujira028@gmail.com>
2020-05-07 08:24:45 +0000

    [add]ja.yml追加(エラー分日本語化のため)

--- app/controllers/trophys_controller.rb
+++ app/controllers/trophys_controller.rb
@@ -4,13 +4,13 @@ class TrophysController < ApplicationController
 
   def create
     # DoneTaskテーブルからclimber_idがcrrent_climber.idと一致するものを探す
-    @done_task = DoneTask.where(climber_id: crrent_climber.id)
+    # @done_task = DoneTask.where(climber_id: crrent_climber.id)
     @new_trophy = Trophy.new
-    if @trophy.save
-      @done_task.each do
-      end
-    end
-    redirect_to request.referer
+    @new_trophy.climber_id = current_climber.id
+    @new_trophy.my_trophy_name = @grade.trophy_name
+    @new_trophy.my_trophy_image_id = @grade.my_trophy_image_id
+    @new_trophy.save
+    redirect_to request.referer, notice: "トロフィー#{@new_trophy.my_trophy_name}を獲得しました！"
   end
 
   private
