b6c1919aacfbdf6aa05d9b026426f98e5e11bb66
sato chiharu <kujira028@gmail.com>
2020-05-09 14:02:52 +0000

    [add]一つのclimber_idに対して同じトロフィー情報を登録させないためのユニーク制約

--- app/models/trophy.rb
+++ app/models/trophy.rb
@@ -1,6 +1,6 @@
 class Trophy < ApplicationRecord
   attachment :my_trophy_image
   belongs_to :climber
-  # 一つのクライマーに対して同じトロフィーを登録させないためのユニーク制約
+  # 一つのclimber_idに対して同じトロフィー情報を登録させないためのユニーク制約
   validates :climber_id, uniqueness: { scope: [:my_trophy_image_id, :my_trophy_name] }
 end
