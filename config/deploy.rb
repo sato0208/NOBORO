a57e7d0f408842cfe9960f7e8766e1c790f35fbb
sato chiharu <kujira028@gmail.com>
2020-05-24 04:25:32 +0000

    [fix]本番環境で画像が表示されないエラー解消

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/sato0208/NOBORO.git"
 set :deploy_to, "/home/ec2-user/NOBORO"
 set :rbenv_ruby, '2.5.7'
 set :linked_files, %w{config/master.key .env}
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "tmp/uploads"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
