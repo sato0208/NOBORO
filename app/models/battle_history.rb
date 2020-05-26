bbf25d706f49213aa26e12dd4e0c0906016711b5
sato chiharu <kujira028@gmail.com>
2020-05-26 11:03:53 +0000

    [fix]本番環境不具合修正

--- app/models/battle_history.rb
+++ app/models/battle_history.rb
@@ -1,7 +1,7 @@
 class BattleHistory < ApplicationRecord
 
   belongs_to :winner, class_name: 'Climber'
-  belongs_to :class_name: 'Climber'
+  belongs_to :loser, class_name: 'Climber'
 
   def result_battle_is_win
     if self.is_draw_status == false
