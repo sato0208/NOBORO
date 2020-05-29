08e4ad457a18d306a6fc2dfb555f2f0b4c69fe87
sato chiharu <kujira028@gmail.com>
2020-05-29 04:31:39 +0000

    [add]テストコードの修正

--- spec/requests/battles_request_spec.rb
+++ spec/requests/battles_request_spec.rb
@@ -2,39 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Battles", type: :request do
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/battles/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/battles/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/battles/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/battles/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/battles/index"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
--- spec/requests/infos_request_spec.rb
+++ spec/requests/infos_request_spec.rb
@@ -2,11 +2,4 @@ require 'rails_helper'
 
 RSpec.describe "Infos", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/infos"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
