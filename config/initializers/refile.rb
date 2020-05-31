e0ba2a1a8f9cb7c82f5d5560de6f3ea1f5b3a108
sato chiharu <kujira028@gmail.com>
2020-05-31 08:18:56 +0000

    [fix]region,bucket修正

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,8 +3,8 @@ require "refile/s3"
 aws = {
   access_key_id: "AWS_ACCESS_KEY_ID",
   secret_access_key: "AWS_SECRET_ACCESS_KEY",
-  region: "AWS_ACCESS_KEY_ID",
-  bucket: "AWS_SECRET_ACCESS_KEY",
+  region: "ap-northeast-1",
+  bucket: "aws-and-infra-noboro",
 }
 Refile.cache = Refile::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::S3.new(prefix: "store", **aws)
