e2784368df3c710c935e3b8997c5d9a9fe0162a9
sato chiharu <kujira028@gmail.com>
2020-06-18 08:54:19 +0000

    [update]notification コントローラのリファクタリング

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -10,9 +10,7 @@ class NotificationsController < ApplicationController
     @update_notification.update(notification_params)
       # binding.pry
     if params[:notification][:confirm_status] == "承認"
-      @update_notification.update(confirm_status: "承認")
-      @update_notification.battle.update(is_valid_status: true)
-      @update_notification.battle.update(finish_at: @update_notification.battle.updated_at.next_day(3))
+      update_notification_is_battle(@update_notification)
       @update_notification.destroy
       redirect_to battles_path, notice: "バトルを承認しました"
     end
@@ -31,12 +29,30 @@ class NotificationsController < ApplicationController
 
   def index
     @notifications = current_climber.passive_notifications.page(params[:page]).per(6)
-    @notifications.where(is_checked_status: false).each do |notification|
+    notifications_checked_status_update(@notifications)
+  end
+
+private
+
+  def notifications_checked_status_update(notifications)
+    notifications.where(is_checked_status: false).each do |notification|
       notification.update(is_checked_status: true)
     end
   end
 
-private
+  def update_notification_is_battle(notification)
+    notification = Notification.find(params[:id])
+    # binding.pry
+      notification.update(
+        confirm_status: "承認",
+        is_checked_status: true
+      )
+      notification.battle.update(
+        finish_at: notification.battle.updated_at.next_day(3),
+        is_valid_status: true,
+      )
+  end
+
   def notification_params
     params.permit(:visiter_id, :visited_id, :confirm_status, :is_checked_status, :battle_id)
   end
