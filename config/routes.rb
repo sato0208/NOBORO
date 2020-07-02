009df91012099b9f9cc2bfdf2b1fa93166ba4e37
sato chiharu <kujira028@gmail.com>
2020-07-02 06:49:12 +0000

    [update]ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,5 @@ Rails.application.routes.draw do
     resources :battles, only: [:create, :destroy, :update, :index]
     resources :notifications, only: [:create, :destroy, :update, :index]
     resources :battle_historys, only: [:create, :index]
+    resources :testsessions, only: :create
 end
---
+++ spec/requests/testsessions_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Testsessions", type: :request do
+
+end
