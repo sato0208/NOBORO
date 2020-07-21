022846d9eac684bea09cf91f0914f2d87e094e6e
sato chiharu <kujira028@gmail.com>
2020-07-21 10:44:27 +0000

    [add]ハンバーガーメニュー実装

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -106,13 +106,11 @@ $(function() {
   });
 });
 
-// ハンバーガーメニュー
-$(function() {
- 
-  $('.menu-trigger').on('click', function() {
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    return false;
-  });
- 
- });
+$(function(){
+	// menu-triggerクリック時に行われる処理
+	$('.menu-trigger').on('click', function() {
+		$(this).toggleClass('active');
+		$('#sp-menu').fadeToggle();
+		return false;
+	});
+});
